a7c2a8129600b3743fdb1bd17c44204cbc60060a
xceptionalbae23 <armsbest@gmail.com>
2024-02-20 17:12:26 +0100

    second attempt

--- src/main/java/com/example/airways/AirWaysApplication.java
+++ src/main/java/com/example/airways/AirWaysApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AirWaysApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AirWaysApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AirWaysApplication.class, args);
+    }
 
 }
